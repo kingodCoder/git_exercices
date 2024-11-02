fd70d3641e4f02e46749fb7612ef89b8253c6ce6
Victor <emmanuelmb261@gmail.com>
2024-11-02 19:59:40 +0200

    Adding circle process panel

--- cm.js
+++ cm.js
@@ -21,10 +21,14 @@
         stroke: 'black',
         strokeWidth: 4,
     });
+    rect.draggable('true');
     layer.add(rect);
     stage.add(layer);
 
+    // Little database
+
     // Adding Shapes
+
     // Adding rect
     squareBtn = $('#launch-square');
     function square(x,y,width,height,fill,stroke='white',strokeWidth=0.001){
@@ -49,11 +53,38 @@
         s = $('#ss').value,
         sw = Number($('#ssw').value);
         sq = square(x,y,w,h,f,s,sw);
+        sq.draggable('true');
         group.add(sq);
         layer.add(group);
     },false);
 
-
+    // Adding circle
     circle = $('#circle');
+    function circle(x,y,radius,fill,stroke='white',strokeWidth=0.001){
+        return new Konva.Rect({
+            x:x,
+            y:y,
+            radius:radius,
+            fill:fill,
+            stroke:stroke,
+            strokeWidth:strokeWidth,
+        });
+    }
+    circle.addEventListener('click', (e)=>{
+        let group = new Konva.Group(),
+        c,
+        x = Number($('#xcpos').value),
+        y = Number($('#ycpos').value),
+        cr = Number($('#cr').value),
+        cf = $('#fc').value,
+        cs = $('#cs').value,
+        csw = Number($('#csw').value);
+        c = square(x,y,r,f,s,sw);
+        c.draggable('true');
+        group.add(c);
+        layer.add(group);
+    },false);
+
+    //Other shapes
     shape = $('#shape');
 })();
